da579caad38d54087ad1c3aa3caa9aa72c0cc5e0
Tony Wallace <tony@tonywallace.com>
2016-10-16 16:37:15 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,318 @@
+# Serialist-Grammar
+
+Serialist-Grammar provides a concise syntax for the expression and transformation of rows in serial music composition.
+
+## Usage
+
+1. Install [PEG.js](http://pegjs.org)
+2. Use PEG.js to [generate a parser](http://pegjs.org/documentation#generating-a-parser) from `serialist-grammar.js`
+3. Use the parser to process input according to the syntax defined below
+4. Map the parser output to your target application
+
+## Syntax
+
+### Rows
+
+Serialist-Grammar accepts rows for pitch class, octave, dynamics and duration. Row types may be placed in any order and used as many times as you want.
+
+#### Pitch Class
+
+Pitch class rows consist of whitespace-delimited pitch classes, surrounded by parenthesis and prefixed with `pc`. Pitch classes are expressed in the traditional notation &mdash; digits `0-9`, `t` for `10` and `e` for `11`.
+
+	pc(0 1 4 t) // parsed as [0, 1, 4, 10]
+
+#### Octave
+
+Octave rows consist of whitespace-delimited signed integer values, surrounded by parenthesis and prefixed with `oct`. `0` should be mapped to an octave around the middle of the pitch range of your target application. Positive and negative values should represent octaves above and below the middle octave, respectively.
+
+	oct(0 1 -2 4) // parsed as [0, 1, -2, 4]
+
+#### Dynamics
+
+Dynamics rows consist of whitespace-delimited float values, surrounded by parenthesis and prefixed with `dyn`. Dynamics values are limited to the range `0-1`.
+
+	dyn(0.5 0.75 1 1.5) // parsed as [0.5, 0.75, 1, 1]
+
+#### Duration
+
+Duration rows consist of whitespace-delimited float values, surrounded by parenthesis and prefixed with `dur`. Duration values should be mapped to a multiple of beat/cycle duration in your target application. (`1` is equal to 1 beat, `0.5` is equal to half a beat, etc.) Values must be positive &mdash; negative values will result in a parse error and `0` values will be filtered.
+
+	dur(1 0.5 0.5 2 0) // parsed as [1, 0.5, 0.5, 2]
+
+### Transformations
+
+Serial-Grammar provides a series of transformations that can be applied to all types of rows. Transformations may be placed in any order and used as many times as you want:
+
+	pc(1 4 0 t 7) @r >> 3 [1 4] @i +3 << 1 *2
+
+The example above does the following:
+
+1. `pc(1 4 0 t 7)`: define a pitch class row
+2. `@r`: use the retrograde form of the row
+3. `>> 3`: rotate the row forward by 3 positions
+4. `[1 4]`: shorten the row by slicing between indexes 1 and 4
+5. `@i`: use the inverted form of the shortened row
+6. `+3`: add 3 to all pitch classes in the row (transpose +3 semitones)
+7. `<< 1`: rotate the row backward by 1 position
+8. `+3`: multiply all pitch classes in the row by 2
+
+#### Row Forms
+
+Row forms are expressed as the `@` symbol followed by `r` and/or `i`, which indicate the retrograde and inverted forms of the row:
+
+* `@r`: retrograde
+* `@i`: inversion
+* `@ri` retrograde followed by inversion (may also be expressed as `@r @i`)
+* `@ir` inversion followed by retrograde (may also be expressed as `@i @r`)
+
+The retrograde form is simply the row in reverse order:
+
+	pc(1 2 3) // reverses to pc(3 2 1)
+
+Inversion behaves differently depending on the type of row. Pitch class rows are inverted within the octave, according to the equation `(12 - value) % 12`:
+
+	pc(1 4 0 t 7) @i // inverts to pc(e 8 0 2 5)
+
+Octave rows are inverted around the middle octave by negating the value:
+
+	oct(1 0 -2) @i // inverts to oct(-1 0 2)
+
+Dynamics rows are inverted within the range `0-1`:
+
+	dyn(1 0.5 0.25 0) @i // inverts to dyn(0 0.5 0.75 1)
+
+Duration rows are inverted according to the equation `1 / value`:
+
+	dur(1 0.5 1.5 2) @i // inverts to dur(1 2 0.6666666666666666 0.5)
+
+#### Rotation
+
+Row rotation is expressed with the `>>` operator for forward rotation and the `<<` operator for backward rotation, followed by the number of positions to rotate by:
+
+	pc(1 2 3 4 5) >> 1 // rotates to pc(2 3 4 5 1)
+	pc(1 2 3 4 5) >> 2 // rotates to pc(3 4 5 1 2)
+	pc(1 2 3 4 5) << 1 // rotates to pc(5 1 2 3 4)
+	pc(1 2 3 4 5) << 2 // rotates to pc(4 5 1 2 3)
+
+The rotation position will wrap within the length of the row, so values greater than the length of the row will still provide useful results:
+
+	pc(1 2 3 4 5) >> 7 // equivalent to pc(1 2 3 4 5) >> (7 % 5) or pc(1 2 3 4 5) >> 2 and rotates to pc(3 4 5 1 2)
+
+#### Slicing
+
+Slicing a row results in a shortened row that contains the values between the start and end indexes of the slice. A slice is expressed as a start index and optional end index, separated by whitespace and surrounded by square brackets:
+
+	pc(1 2 3 4 5) [1 4] // slices to [2 3 4]
+
+Omit the end index to slice from the start index to the end of the row:
+
+	pc(1 2 3 4 5) [2] // slices to pc(3 4 5)
+
+Slicing uses [Array.prototype.slice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice), so the value at the end index is _not_ included in the slice.
+
+#### Math Expressions
+
+Serialist-Grammar supports addition, subtraction, multiplication and division expressions with the following limitations:
+
+1. In pitch class rows, the result will be wrapped within the normal pitch class range (`0-11`) and rounded to the nearest integer
+2. In octave rows, the result will be rounded to the nearest integer
+3. In dynamics rows, the result will be always limited to the range `0-1`
+4. In duration rows, the result will be limited to a minimum value of `0` and `0` values are filtered
+5. Division by `0` expressions will be ignored
+
+Examples:
+
+	pc(1 4 5 8) + 3 // results in pc(4 7 8 e)
+	pc(1 4 5 8) * 2 // results in pc(2 8 t 4) after wrapping within 0-11
+	dyn(0.5 0.25 0.75 2) - 0.5 // results in dyn(0 0 0.25 1) after limiting to 0-1
+	dur(1 0.5 0.5 2) - 1 // results in pc(1) after limiting to a minimum of zero and filtering zero
+	dur(1 0.5 0.5) / 0 // results in dur(1 0.5 0.5) because division by zero is ignored
+
+### Identifiers
+
+Each sequence of rows may be given an arbitrary identifier consisting of alphanumeric characters wrapped in parenthesis and prefixed with `id`:
+
+	id(sequence1) pc(1 2 5)
+
+### Flags
+
+Flags may be added to the beginning of the input to control parser behaviour.
+
+Currently, the only supported flag is `flat`, which changes the output format. Without the `flat` flag, the output closely matches the input. Notice that the two pitch class rows are represented by separate arrays:
+
+	id(sequence1) pc(1 2 5) pc(7 8 t) oct(0 2) dyn(0.75 0.5 0.25) dur(1 0.5 0.5)
+
+	// Output:
+	/*
+	[
+		[
+			[
+		    	"id",
+	     		"sequence1"
+		  	],
+		  	[
+		    	"pc",
+	     		[
+	        		1,
+	        		2,
+	        		5
+	     		]
+		  	],
+		  	[
+		    	"pc",
+		     	[
+		        	7,
+		        	8,
+		        	10
+		     	]
+		  	],
+		  	[
+		    	"oct",
+		     	[
+		        	0,
+		       		2
+		     	]
+		  	],
+		  	[
+		    	"dyn",
+		    	[
+		        	0.75,
+		        	0.5,
+		        	0.25
+		    	]
+		  	],
+		  	[
+		    	"dur",
+		    	[
+		        	1,
+		        	0.5,
+		        	0.5
+		     	]
+		  	]
+		]
+	]
+	*/
+
+The `flat` flag simplifies the output by formatting each voice as an object with one member per row type. Multiple rows of the same type will be concatenated in the order in which they appear in the input:
+
+	flat
+	id(sequence1) pc(1 2 5) pc(7 8 t) oct(0 2) dyn(0.75 0.5 0.25) dur(1 0.5 0.5)
+
+	// Output:
+	[
+		{
+		  	"id": "sequence1",
+		  	"pc": [
+		     	1,
+		     	2,
+		     	5,
+		     	7,
+		     	8,
+		     	10
+		  	],
+		  	"oct": [
+		     	0,
+		     	2
+		  	],
+		  	"dyn": [
+		     	0.75,
+		     	0.5,
+		     	0.25
+		  	],
+		  	"dur": [
+		     	1,
+		     	0.5,
+		     	0.5
+		  	]
+		}
+	]
+
+### Multiple Voices
+
+Multiple voices may be defined by separating sequences of rows with a comma and newline. Each voice will appear as a separate object in the output:
+
+	flat
+	id(sequence1) pc(1 2 5) dur(1 0.5),
+	id(sequence2) pc(3 4 7) dur(0.5 0.25)
+
+	// Output:
+	/*
+	[
+		{
+		  	"id": "sequence1",
+		  	"pc": [
+		    	1,
+		     	2,
+		     	5
+		  ],
+		  "oct": [],
+		  "dyn": [],
+		  "dur": [
+		     1,
+		     0.5
+		  ]
+		},
+		{
+		  "id": "sequence2",
+		  "pc": [
+		     3,
+		     4,
+		     7
+		  ],
+		  "oct": [],
+		  "dyn": [],
+		  "dur": [
+		     0.5,
+		     0.25
+		  ]
+		}
+	]
+	*/
+
+### Whitespace &amp; Line Breaks
+
+Serialist-Grammar is very accommodating of whitespace (or lack thereof). All of the following inputs should be valid and produce identical output:
+
+	// 1.
+
+	id(sequence1) pc(1 2 5) + 2 pc(7 8 t) << 1 oct(0 2) / 2 dyn(0.75 0.5 0.25) [1] dur(1 0.5 0.5) * 2,
+	id(sequence2) pc(t 1 2) << 2 oct(3 5 4 2) * 3 dyn(1 0.5) - 0.25 dur(0.5 0.25 0.25)
+
+	// 2.
+
+	id(sequence1) pc(1 2 5)+2 pc(7 8 t)<<1 oct(0 2)/2 dyn(0.75 0.5 0.25)[1] dur(1 0.5 0.5)*2,
+	id(sequence2) pc(t 1 2)<<2 oct(3 5 4 2)*3 dyn(1 0.5)-0.25 dur(0.5 0.25 0.25)
+
+	// 3.
+
+	id(sequence1)
+	pc(1 2 5) +2
+	pc(7 8 t) <<1
+	oct(0 2) /2
+	dyn(0.75 0.5 0.25) [1]
+	dur(1 0.5 0.5) * 2,
+
+	id(sequence2)
+	pc(t 1 2) <<2
+	oct(3 5 4 2) *3
+	dyn(1 0.5) - 0.25
+	dur(0.5 0.25 0.25)
+
+	// 4.
+
+	id(sequence1)
+		pc(1 2 5) +2
+		pc(7 8 t) <<1
+		oct(0 2) /2
+		dyn(0.75 0.5 0.25) [1]
+		dur(1 0.5 0.5) * 2,
+
+	id(sequence2)
+		pc(t 1 2) <<2
+		oct(3 5 4 2) *3
+		dyn(1 0.5) - 0.25
+		dur(0.5 0.25 0.25)
+
+## License
+
+Serialist-Grammar is made available under the terms of the GNU General Public License v3.0 (or greater).
---
+++ serialist-grammar.js
@@ -0,0 +1,576 @@
+/*
+Serialist-Grammar provides a concise syntax for the expression and transformation of pitch class, octave, dynamics and duration rows in serial music composition.
+Copyright (C) 2016  Irritant Creative Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* **************** */
+/* Global Functions */
+/* **************** */
+
+{
+    // Voice:
+
+    function flatten_voice(voice) {
+        if (voice.length === 0) {
+            return null;
+        }
+
+        var flat = {
+            id: '',
+            pc: [],
+            oct: [],
+            dyn: [],
+            dur: []
+        };
+
+        voice.forEach(function(element) {
+            var type = element[0];
+            var val = element[1];
+
+            if (Array.isArray(val)) {
+                flat[type] = flat[type].concat(val);
+            } else {
+                flat[type] = val;
+            }
+        });
+
+        return flat;
+    }
+
+    // Pitch class rows:
+
+    function apply_pitch_class_row_transforms(row, transforms) {
+        transforms.forEach(function(t) {
+            var transform_name = t[0];
+            var transform_args = t[1];
+
+            // Evaluate forms:
+            if (transform_name == 'forms') {
+                row = apply_row_forms(row, transform_args, function(f) {
+                    // Round the result:
+                    return Math.round(12 - f)  % 12;
+                });
+            }
+
+            // Evaluate math:
+            if (transform_name == 'math') {
+                row = apply_row_math(row, transform_args, function(f) {
+                    // Round the result:
+                    return Math.round(f)  % 12;
+                });
+            }
+
+            // Evaluate rotations:
+            if (transform_name == 'rotation') {
+                row = apply_row_rotation(row, transform_args);
+            }
+
+            // Evaluate slices:
+            if (transform_name == 'slice') {
+                row = apply_row_slice(row, transform_args);
+            }
+
+        });
+
+        return row;
+    }
+
+    // Octave rows:
+
+    function apply_octave_row_transforms(row, transforms) {
+        transforms.forEach(function(t) {
+            var transform_name = t[0];
+            var transform_args = t[1];
+
+            // Evaluate forms:
+            if (transform_name == 'forms') {
+                row = apply_row_forms(row, transform_args, function(f) {
+                    // Round the result:
+                    return Math.round(-f);
+                });
+            }
+
+            // Evaluate math:
+            if (transform_name == 'math') {
+                row = apply_row_math(row, transform_args, function(f) {
+                    // Round the result:
+                    return Math.round(f);
+                });
+            }
+
+            // Evaluate rotations:
+            if (transform_name == 'rotation') {
+                row = apply_row_rotation(row, transform_args);
+            }
+
+            // Evaluate slices:
+            if (transform_name == 'slice') {
+                row = apply_row_slice(row, transform_args);
+            }
+
+        });
+
+        return row;
+    }
+
+    // Dynamics rows:
+
+    function apply_dynamics_row_transforms(row, transforms) {
+        transforms.forEach(function(t) {
+            var transform_name = t[0];
+            var transform_args = t[1];
+
+            // Evaluate forms:
+            if (transform_name == 'forms') {
+                row = apply_row_forms(row, transform_args, function(f) {
+                    // Limit the result:
+                    return Math.min(1.0, Math.max(1.0 - f, 0.0));
+                });
+            }
+
+            // Evaluate math:
+            if (transform_name == 'math') {
+                row = apply_row_math(row, transform_args, function(f) {
+                    // Limit the result:
+                    return Math.min(1.0, Math.max(f, 0.0));
+                });
+            }
+
+            // Evaluate rotations:
+            if (transform_name == 'rotation') {
+                row = apply_row_rotation(row, transform_args);
+            }
+
+            // Evaluate slices:
+            if (transform_name == 'slice') {
+                row = apply_row_slice(row, transform_args);
+            }
+
+        });
+
+        return row;
+    }
+
+    // Duration rows:
+
+    function apply_duration_row_transforms(row, transforms) {
+        transforms.forEach(function(t) {
+            var transform_name = t[0];
+            var transform_args = t[1];
+
+            // Evaluate forms:
+            if (transform_name == 'forms') {
+                row = apply_row_forms(row, transform_args, function(f) {
+                    // Limit the result:
+                    return Math.max(1.0 / f, 0);
+                });
+            }
+
+            // Evaluate math:
+            if (transform_name == 'math') {
+                row = apply_row_math(row, transform_args, function(f) {
+                    // Limit the result:
+                    return Math.max(f, 0);
+                })
+                .filter(function(f) {
+                    // Filter zero and negative values:
+                    return f > 0;
+                });
+            }
+
+            // Evaluate rotations:
+            if (transform_name == 'rotation') {
+                row = apply_row_rotation(row, transform_args);
+            }
+
+            // Evaluate slices:
+            if (transform_name == 'slice') {
+                row = apply_row_slice(row, transform_args);
+            }
+
+        });
+
+        return row;
+    }
+
+    // Row forms:
+
+    function apply_row_forms(row, args, inversion_transform) {
+        args.forEach(function(form) {
+            switch(form) {
+                case 'r':
+                    row = row.reverse();
+                    break;
+                case 'i':
+                    row = row.map(function(f) {
+                        if (typeof inversion_transform == 'function') {
+                            return inversion_transform(f);
+                        } else {
+                            return f;
+                        }
+                    });
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        return row;
+    }
+
+    // Row math:
+
+    function apply_row_math(row, args, value_transform) {
+        var op = args[0];
+        var val = args[1];
+
+        row = row.map(function(f) {
+            switch (op) {
+                case '+':
+                    f += val;
+                    break;
+                case '-':
+                    f -= val;
+                    break;
+                case '*':
+                    f *= val;
+                    break;
+                case '/':
+                    f /= (val !== 0) ? val : 1;
+                    break;
+                default:
+                    break;
+            }
+
+            if (typeof value_transform == 'function') {
+                return value_transform(f);
+            } else {
+                return f;
+            }
+        });
+
+        return row;
+    }
+
+    // Row rotation:
+
+    function apply_row_rotation(row, args) {
+        var op = args[0];
+        var val = args[1] % row.length;
+
+        switch (op) {
+            case '<<':
+                var head = row.slice(0, row.length - val);
+                var tail = row.slice(row.length - val, row.length);
+                row = tail.concat(head);
+                break;
+            case '>>':
+                var head = row.slice(0, val);
+                var tail = row.slice(val, row.length);
+                row = tail.concat(head);
+                break;
+            default:
+                break;
+        }
+
+        return row;
+    }
+
+    // Row slice:
+
+    function apply_row_slice(row, args) {
+        var start = args[0];
+        var end = args[1];
+        return row.slice(start, end);
+    }
+}
+
+/* ******* */
+/* Grammar */
+/* ******* */
+
+start = (
+    flags: flag*
+    voices:voice_sequence
+    {
+        flags.forEach(function(flag) {
+            switch (flag) {
+                case 'flat':
+                    voices = voices.map(function(voice) {
+                        return flatten_voice(voice);
+                    });
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        return voices;
+    }
+)
+
+flag = (
+    flag: 'flat' space_or_line_break
+    {
+        return flag;
+    }
+)
+
+voice_sequence = (
+    head: (voice:voice voice_separator { return voice; })*
+    tail: ((voice:voice { return voice; }) / voice_separator)
+    {
+        return head.concat([tail]);
+    }
+)
+
+voice = (
+    voice_id_label /
+    pitch_class_row_label /
+    octave_row_label /
+    dynamics_row_label /
+    duration_row_label
+)*
+
+// Voice identifier:
+
+voice_id_label = (
+    label: 'id' space_or_line_break
+    value: voice_id_value space_or_line_break
+    {
+        return [label, value];
+    }
+)
+
+voice_id_value = (
+    '(' space_or_line_break value: $[a-zA-Z0-9]+ space_or_line_break ')'
+    {
+        return value;
+    }
+)
+
+// Pitch class row:
+
+pitch_class_row_label = (
+    label: 'pc' space_or_line_break
+    row: (pitch_class_row_transformed space_or_line_break)?
+    {
+        return [label, row[0]];
+    }
+)
+
+pitch_class_row_transformed = (
+    row: pitch_class_row space*
+    transforms: (t:row_transform space* { return t; })*
+    {
+        if (transforms) {
+            row = apply_pitch_class_row_transforms(row, transforms);
+        }
+
+        return row;
+    }
+)
+
+pitch_class_row = (
+    '(' space*
+    head: (pc:pitch_class space+ { return pc; })*
+    tail: ((pc:pitch_class { return pc; }) / space*)
+    ')'
+    {
+        return head.concat(tail);
+    }
+)
+
+// Octave row:
+
+octave_row_label = (
+    label: 'oct' space_or_line_break
+    row: (octave_row_transform space_or_line_break)?
+    {
+        return [label, row[0]];
+    }
+)
+
+octave_row_transform = (
+    row: octave_row space*
+    transforms: (t:row_transform space* { return t; })*
+    {
+        if (transforms) {
+            row = apply_octave_row_transforms(row, transforms);
+        }
+
+        return row;
+    }
+)
+
+octave_row = (
+    '(' space*
+    head: (i:signed_int space+ { return i; })*
+    tail: ((i:signed_int { return i; }) / space*)
+    ')'
+    {
+        return head.concat(tail);
+    }
+)
+
+// Dynamics row:
+
+dynamics_row_label = (
+    label: 'dyn' space_or_line_break
+    row: (dynamics_row_transform space_or_line_break)?
+    {
+        return [label, row[0]];
+    }
+)
+
+dynamics_row_transform = (
+    row: dynamics_row space*
+    transforms: (t:row_transform space* { return t; })*
+    {
+        if (transforms) {
+            row = apply_dynamics_row_transforms(row, transforms);
+        }
+
+        return row;
+    }
+)
+
+dynamics_row = (
+    '(' space*
+    head: (f:float space+ { return f; })*
+    tail: ((f:float { return f; }) / space*)
+    ')'
+    {
+        return head.concat(tail).map(function(f) {
+            // Limit to range 0-1:
+            return Math.min(1.0, Math.max(f, 0.0));
+        });
+    }
+)
+
+// Duration row:
+
+duration_row_label = (
+    label: 'dur' space_or_line_break
+    row: (duration_row_transform space_or_line_break)?
+    {
+        return [label, row[0]];
+    }
+)
+
+duration_row_transform = (
+    row: duration_row space*
+    transforms: (t:row_transform space* { return t; })*
+    {
+        if (transforms) {
+            row = apply_duration_row_transforms(row, transforms);
+        }
+
+        return row;
+    }
+)
+
+duration_row = (
+    '(' space*
+    head: (f:float space+ { return f; })*
+    tail: ((f:float { return f; }) / space*)
+    ')'
+    {
+        return head.concat(tail).filter(function(f) {
+            // Filter zero and negative values:
+            return f > 0.0;
+        });
+    }
+)
+
+// Row transforms:
+
+row_transform = (
+    row_forms /
+    row_rotation /
+    row_slice /
+    row_math
+)
+
+row_forms = (
+    '@' space*
+    form: ('r' / 'i')*
+    {
+        return ['forms', form];
+    }
+)
+
+row_rotation = (
+    op: ('<<' / '>>') space*
+    val: int
+    {
+        return ['rotation', [op, val]];
+    }
+)
+
+row_slice = (
+    '[' space*
+    head: (i:int space+ { return i; })*
+    tail: ((i:int { return i; }) / space*)
+    ']'
+    {
+        return ['slice', head.concat(tail)];
+    }
+)
+
+row_math = (
+    space*
+    op: ('+'/'-'/'*'/'/') space*
+    val: all_types
+    {
+        return ['math', [op, val]];
+    }
+)
+
+// Types:
+
+all_types = (
+    float /
+    int /
+    signed_int /
+    pitch_class
+)
+
+pitch_class = (
+    pc: [0-9te]
+    {
+        switch(pc) {
+            case 't':
+                return 10;
+            case 'e':
+                return 11;
+            default:
+                return +pc % 12;
+        }
+    }
+)
+
+float = f:$([0-9]* dot? [0-9]*) { return isNaN(f) ? 0 : +f }
+signed_int = sign:'-'? i:int { return (sign) ? 0 - (+i) : +i; }
+int = i:$([0-9]+) { return +i }
+
+// Punctuation, whitespace and breaks:
+
+dot = '.'
+space = ' ' / '\t'
+line_break = '\n'
+space_or_line_break = space* line_break* space*
+voice_separator = space_or_line_break ',' space_or_line_break
